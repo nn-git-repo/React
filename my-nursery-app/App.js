6c81b4b35b9e905484065b010470c926e1a308e8
Namratha <162692513+nn-git-repo@users.noreply.github.com>
2025-08-19 09:05:33 +0530

    App.js

---
+++ my-nursery-app/App.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import Header from './components/Header';
+import First from './components/First';
+import {Footer} from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     <First />
+      <Footer />
+      
+    </div>
+  );
+}
+
+export default App;
