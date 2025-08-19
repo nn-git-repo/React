fd1b58616d87b76a11b6ee59420cdf8cc496198c
Namratha <162692513+nn-git-repo@users.noreply.github.com>
2025-08-19 08:59:26 +0530

    App.js

---
+++ Cloth-store/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ClothStore from './components/ClothStore';
+
+function App() {
+  return (
+    <div className="App">
+     <ClothStore />
+    </div>
+  );
+}
+
+export default App;
