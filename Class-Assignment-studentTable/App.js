a7601c0b2d230e784508de87c4969ba101cb8e1a
Namratha <162692513+nn-git-repo@users.noreply.github.com>
2025-08-19 09:20:30 +0530

    App.js

---
+++ Class-Assignment-studentTable/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+
+import StudentTable from './components/StudentTable';
+
+function App() {
+  return (
+    <div >
+    
+    <StudentTable />
+    </div>
+  );
+}
+
+export default App;
