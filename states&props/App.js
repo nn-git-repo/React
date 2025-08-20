497733e41d318b98dfe57886f707afd6aaa3247b
Namratha <162692513+nn-git-repo@users.noreply.github.com>
2025-08-20 14:42:16 +0530

    Update App.js

--- states&props/App.js
+++ states&props/App.js
@@ -1,1 +1,22 @@
 
+import './App.css';
+import CourseApp from './components/E-LearningPlatform';
+import FitnessApp from './components/FitnessTrackerDashboard';
+import FoodOrderApp from './components/FoodOrder';
+import HotelBookingApp from './components/HotelRoomReservation';
+import MovieBookingApp from './components/MovieTicketBooking';
+
+function App() {
+  return (
+    <div className="App">
+      <FoodOrderApp />
+      <CourseApp  />
+      <MovieBookingApp />
+      <FitnessApp />
+      <HotelBookingApp />
+    </div>
+  );
+}
+
+export default App;
+
