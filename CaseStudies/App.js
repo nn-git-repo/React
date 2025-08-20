f570fd80ab36c070462b662df38ccd03d1957a2d
Namratha <162692513+nn-git-repo@users.noreply.github.com>
2025-08-20 16:34:07 +0530

    Update App.js

--- CaseStudies/App.js
+++ CaseStudies/App.js
@@ -1,1 +1,21 @@
 
+import logo from './logo.svg';
+import './App.css';
+
+import FormHandling from './components/CaseStudies/FormHandling';
+import ThemeSwitching from './components/CaseStudies/ThemeSwitching';
+import UpdateTitle from './components/CaseStudies/UpdateTitle';
+
+function App() {
+  return (
+    <div className="App">
+     
+
+       <FormHandling />
+       <ThemeSwitching />
+       <UpdateTitle />
+    </div>
+  );
+}
+
+export default App;
