5c757d966bd89cd78e1559a4bb838e2d90744ea7
Namratha <162692513+nn-git-repo@users.noreply.github.com>
2025-08-22 17:24:59 +0530

    App.jsx

---
+++ ReactRouter/App.jsx
@@ -0,0 +1,16 @@
+
+import BookExplorer from './components/BookExplorer'
+// import ProductList from './components/ProductList'
+
+function App() {
+ 
+
+  return (
+    <>
+     {/* <ProductList /> */}
+     <BookExplorer />
+    </>
+  )
+}
+
+export default App
