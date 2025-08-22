efe85c66e9a27620cc18c002421983edc4c27615
Namratha <162692513+nn-git-repo@users.noreply.github.com>
2025-08-22 13:06:25 +0530

    App.js

---
+++ ControlledComponent/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import FeedbackForm from './components/FeedbackForm';
+
+
+
+function App() {
+  return (
+    <div className="App">
+   
+     <FeedbackForm />
+
+    
+    </div>
+  );
+}
+
+export default App;
