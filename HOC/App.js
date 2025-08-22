e87ef62a7fcebfb592a5b4f9d30a844f6ca7ce4b
Namratha <162692513+nn-git-repo@users.noreply.github.com>
2025-08-22 12:55:42 +0530

    Update App.js

--- HOC/App.js
+++ HOC/App.js
@@ -1,1 +1,17 @@
 
+
+import './App.css';
+import EmployeeHOC from './components/HOC/Employee HOC/EmployeeHOC';
+import ExampleFeedbackApp from './components/HOC/Employee HOC/FeedbackForm';
+
+
+function App() {
+  return (
+    <div className="App">
+    
+     <ExampleFeedbackApp />
+    </div>
+  );
+}
+
+export default App;
